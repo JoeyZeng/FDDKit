ffbe1054fd5d39d11e8de87f5dec1eb5ab2ddc69
JoeyZeng <zzying77@qq.com>
2014-11-19 17:26:39 +0800

    增加工程

---
+++ Products/FDDKit.framework/Headers/FDDKit.h
@@ -0,0 +1,19 @@
+//
+//  FDDKit.h
+//  FDDKit
+//
+//  Created by JoeyZeng on 14/11/18.
+//  Copyright (c) 2014年 FangDD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FDDKit.
+FOUNDATION_EXPORT double FDDKitVersionNumber;
+
+//! Project version string for FDDKit.
+FOUNDATION_EXPORT const unsigned char FDDKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDDKit/PublicHeader.h>
+
+
---
+++ Products/FDDKit.framework/Headers/testIV.h
@@ -0,0 +1,13 @@
+//
+//  testIV.h
+//  FDDKit
+//
+//  Created by JoeyZeng on 14/11/19.
+//  Copyright (c) 2014年 FangDD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testIV : UIImageView
+
+@end
